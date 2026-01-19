c48ef44a0a810ec56bced25e29af995d4c7ea989
Ivan Omelchenko <omelchenkoivan96@gmail.com>
2026-01-19 17:59:41 +0200

    Switch to reflexded linux and to Debian Trixie
    Signed-off-by: Ivan Omelchenko <omelchenkoivan96@gmail.com>

--- _internal/reflexded/install_reflexded.sh
+++ _internal/reflexded/install_reflexded.sh
@@ -9,29 +9,26 @@ fi
 
 cd /srv/steam/
 
-wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz && tar -xf steamcmd_linux.tar.gz
+mkdir -p reflexded
 
 ./steamcmd.sh <<'EOF'
-@ShutdownOnFailedCommand 1
-@sSteamCmdForcePlatformType windows
-@NoPromptForPassword 1
-force_install_dir /srv/steam/reflexded
-login anonymous
-app_update 329740 validate
-quit
+    @ShutdownOnFailedCommand 1
+    @sSteamCmdForcePlatformType linux
+    @NoPromptForPassword 1
+    force_install_dir /srv/steam/reflexded
+    login anonymous
+    app_update 329740 -beta beta -betapassword betabetabeta validate
+    quit
 EOF
 
 # explicit check, as steamcmd sometimes returns 0 even on errors
 # at least in running with arguments format, the stdin seems to be fine
-test -f /srv/steam/reflexded/reflexded.exe || exit 1
+test -f /srv/steam/reflexded/reflexded || exit 1
 
 cd reflexded
-cp /reflexded_fixed.exe reflexded.exe
 
-chmod o+x reflexded.exe
+chmod o+x reflexded
+mkdir -pm 755 replays
 
-# -p is not just for creating parents, it's for being silent, if the dir is already there
-mkdir -p replays
-chmod 755 replays
 
 touch $SENTINEL
--- _internal/reflexded/runserver.sh
+++ _internal/reflexded/runserver.sh
@@ -3,13 +3,5 @@ set -euo pipefail
 
 cd /srv/steam/reflexded
 
-# sleep $((RANDOM % 3 + 1))
-# that sleep was an attempt to fix a rare error,
-# when after frequent restarts, there appear some odd errors
-# regarding something about steam and certificates
-# which felt like rate limit on master server registration or VAC auth
-# but it's too rare, and I'm not sure, so whatever...
-
-# exec and handling of CTRL+C seems to have no effect here...
-export WINEDEBUG=-all
-exec wine ./reflexded.exe $SRV_ARGS_COMMON $SRV_ARGS
+export LD_LIBRARY_PATH=.:./linux64
+exec ./reflexded $SRV_ARGS_COMMON $SRV_ARGS
