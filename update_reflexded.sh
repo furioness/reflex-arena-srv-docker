02386a2a77dd895812ec1e06f40703e20ac30687
Ivan Omelchenko <omelchenkoivan96@gmail.com>
2026-01-30 18:33:51 +0200

    Make the update script print logs and fail on errors
    Signed-off-by: Ivan Omelchenko <omelchenkoivan96@gmail.com>

--- update_reflexded.sh
+++ update_reflexded.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 #   Steam> Update state (0x0) unknown, progress: 0.00 (0 / 0)
 #   Error! App '329740' state is 0x6 after update job.
@@ -7,4 +8,5 @@ sudo rm -f reflexded/steamapps/appmanifest_329740.acf
 
 sudo rm -f reflexded/INSTALL_FINISHED_SENTINEL
 docker compose stop
+docker compose up reflexded_installer  # so we can see the logs
 docker compose up -d
